0290df3c937ffc095d2d0d5fd6688dd9f5d49b36
Jérôme TAMARELLE <jerome@tamarelle.net>
2018-05-03 11:19:53 +0200

    Implement function
    Algorithm from https://github.com/banterfm/graphql-crunch/blob/v1.1.2/src/crunch.js

--- src/functions.php
+++ src/functions.php
@@ -2,12 +2,71 @@
 
 namespace GromNaN\JsonCrunch;
 
-function crunch($uncrunched)
+function uncrunch($values)
 {
-  return array($uncrunched);
+    $expanded = new \SplFixedArray(count($values));
+    $lookup = function ($i) use ($expanded) {
+        return $expanded[$i];
+    };
+
+    foreach ($values as $i => $value) {
+        $expanded[$i] = is_array($value) ? array_map($lookup, $value) : $value;
+    }
+
+    return end($expanded);
 }
 
-function uncrunch($crunched)
+function crunch($data)
 {
-  return $crunched[0];
+    $index = new \ArrayObject();
+    $values = new \ArrayObject();
+
+    crunch_flatten($data, $index, $values);
+
+    return (array) $values;
+}
+
+/**
+ * @internal
+ *
+ * @param  [type]       $data   [description]
+ * @param  \ArrayObject $index  [description]
+ * @param  \ArrayObject $values [description]
+ * @return [type]               [description]
+ */
+function crunch_flatten($data, \ArrayObject $index, \ArrayObject $values)
+{
+    $recurse = function ($data) use ($index, $values) {
+        return crunch_flatten($data, $index, $values);
+    };
+
+    $flattened = is_array($data) ? array_map($recurse, $data) : $data;
+
+    return crunch_insert($flattened, $index, $values);
+}
+
+/**
+ * @internal
+ *
+ * @param  [type]       $value  [description]
+ * @param  \ArrayObject $index  [description]
+ * @param  \ArrayObject $values [description]
+ * @return [type]               [description]
+ */
+function crunch_insert($value, \ArrayObject $index, \ArrayObject $values)
+{
+    if (is_array($value)) {
+        ksort($value);
+    }
+    $key = json_encode($value);
+
+    if (!$index->offsetExists($key)) {
+        $i = $index->count();
+        $index->offsetSet($key, $i);
+        $values->append($value);
+
+        return $i;
+    }
+
+    return $index->offsetGet($key);
 }
--- tests/CrunchTest.php
+++ tests/CrunchTest.php
@@ -13,99 +13,98 @@ class CrunchTest extends TestCase
         yield [
             'null primitive',
             null,
-            [null]
+            [null],
         ];
 
         yield [
             'number primitive',
             0,
-            [0]
+            [0],
         ];
 
         yield [
             'boolean primitive',
             true,
-            [true]
+            [true],
         ];
 
         yield [
             'string primitive',
             "string",
-            ["string"]
+            ["string"],
         ];
 
         yield [
             'empty array',
             [],
-            [[]]
+            [[]],
         ];
 
         yield [
             'single-item array',
             [null],
-            [null, [0]]
+            [null, [0]],
         ];
 
         yield [
             'multi-primitive all distinct array',
             [null, 0, true, "string"],
-            [null, 0, true, "string", [0, 1, 2, 3]]
+            [null, 0, true, "string", [0, 1, 2, 3]],
         ];
 
         yield [
             'multi-primitive repeated array',
             [true, true, true, true],
-            [true, [0, 0, 0, 0]]
+            [true, [0, 0, 0, 0]],
         ];
 
         yield [
             'one-level nested array',
             [[1,2,3]],
-            [1, 2, 3, [0, 1, 2], [3]]
+            [1, 2, 3, [0, 1, 2], [3]],
         ];
 
         yield [
             'two-level nested array',
             [[[1,2,3]]],
-            [1, 2, 3, [0, 1, 2], [3], [4]]
+            [1, 2, 3, [0, 1, 2], [3], [4]],
         ];
 
         yield [
             'empty object',
             new \stdClass,
-            [new \stdClass]
+            [new \stdClass],
         ];
 
         yield [
             'single-item object',
             ['a' => null],
-            [null, ['a' => 0]]
+            [null, ['a' => 0]],
         ];
 
         yield [
             'multi-item all distinct object',
             ['a' => null, 'b' => 0, 'c' => true, 'd' => "string"],
-            [null, 0, true, "string", ['a' => 0, 'b' => 1, 'c' => 2, 'd' => 3]]
+            [null, 0, true, "string", ['a' => 0, 'b' => 1, 'c' => 2, 'd' => 3]],
         ];
 
         yield [
             'multi-item repeated object',
             ['a' => true, 'b' => true, 'c' => true, 'd' => true],
-            [true, ['a' => 0, 'b' => 0, 'c' => 0, 'd' => 0]]
+            [true, ['a' => 0, 'b' => 0, 'c' => 0, 'd' => 0]],
         ];
 
         yield [
             'complex array',
             [['a' => true, 'b' => [1, 2, 3]], [1,2,3]],
-            [true, 1, 2, 3, [1, 2, 3], ['a' => 0, 'b' => 4], [5, 4]]
+            [true, 1, 2, 3, [1, 2, 3], ['a' => 0, 'b' => 4], [5, 4]],
         ];
-/*
+
         yield [
             'complex object',
-            ['a' => true, 'b' => [1, 2, 3], 'c' => ['a' => true, 'b' => [1,2,3]],
-            [true, 1, 2, 3, [1, 2, 3], ['a' => 0, 'b' => 4], ['a' => 0, 'b' => 4, 'c' => 5]]
+            ['a' => true, 'b' => [1, 2, 3], 'c' => ['a' => true, 'b' => [1,2,3]]],
+            [true, 1, 2, 3, [1, 2, 3], ['a' => 0, 'b' => 4], ['a' => 0, 'b' => 4, 'c' => 5]],
         ];
-*/
     }
 
     /**
